35eda96fb50411aac96d78bf27e738b88931da7b
gal.green <gal.green@overwolf.com>
2020-02-01 01:28:46 +0200

    new cracks algorithm (very inefficient currently)

--- Assets/Scripts/MaskController.cs
+++ Assets/Scripts/MaskController.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class MaskController : MonoBehaviour
 {
-    public int BlobRadius = 40;
+    public int BlobRadius = 14;
     private RenderTexture cracksMaskRenderTexture;
     private Texture2D cracksMaskTexture;
-    public Vector2Int[] crackBlob;
+    private Vector3Int[] crackBlobs;
 
     void Start()
     {        
@@ -15,23 +15,23 @@ public class MaskController : MonoBehaviour
         cracksMaskTexture = new Texture2D(this.cracksMaskRenderTexture.width, this.cracksMaskRenderTexture.height);
         Renderer renderer = GetComponent<Renderer>();
         renderer.materials[1].SetTexture("_OpacityTex", cracksMaskTexture);
-
-        crackBlob = new Vector2Int[40];
-        for (int i = 0; i < 40; i++)
-        {
-            crackBlob[i] = new Vector2Int(Random.Range(0, cracksMaskTexture.width), Random.Range(0, cracksMaskTexture.height));
-        }
-        refreshCracksMaskByBlobs();
     }
 
-    float latestUpdateTime;
     void Update()
     {        
-        if (Time.time - latestUpdateTime > 0.5f)
+        
+    }
+
+    public void SetBlobs(List<Vector3> blobs, int blobRadius)
+    {
+        this.BlobRadius = blobRadius;
+        List<Vector3Int> absoluteBlobs = new List<Vector3Int>();
+        foreach (var blob in blobs)
         {
-            latestUpdateTime = Time.time;
-            refreshCracksMaskByBlobs();
+            absoluteBlobs.Add(new Vector3Int((int)(blob.x * cracksMaskTexture.width), (int)(blob.y * cracksMaskTexture.height), (int)blob.z));
         }
+        crackBlobs = absoluteBlobs.ToArray();
+        refreshCracksMaskByBlobs();
     }
 
     private void refreshCracksMaskByBlobs()
@@ -39,15 +39,15 @@ public class MaskController : MonoBehaviour
         RenderTexture.active = cracksMaskRenderTexture;
         cracksMaskTexture.ReadPixels(new Rect(0, 0, cracksMaskRenderTexture.width, cracksMaskRenderTexture.height), 0, 0);
 
-        foreach (var blob in crackBlob)
+        foreach (var blob in crackBlobs)
         {
             for (int x = Mathf.Max(blob.x - BlobRadius, 0); x < Mathf.Min(blob.x + BlobRadius, cracksMaskTexture.width); x++)
             {
                 for (int y = Mathf.Max(blob.y - BlobRadius, 0); y < Mathf.Min(blob.y + BlobRadius, cracksMaskTexture.height); y++)
                 {
-                    if (Vector2.Distance(new Vector2(x, y), blob) < BlobRadius)
+                    if (Vector2.Distance(new Vector2(x, y), new Vector2Int(blob.x, blob.y)) < BlobRadius)
                     {
-                        cracksMaskTexture.SetPixel(x, y, new Color(1, 0, 0));
+                        cracksMaskTexture.SetPixel(x, y, new Color(1, 0, 0, blob.z / 100f));
                     }
                 }
             }

85c2617dcd582ddbb27c1e132b8ed50a1fced5d3
helena.gri <helena.gri@gmail.com>
2020-01-31 18:45:52 +0200

    added the GlueGun model + main folders in project inspector

---
+++ Assets/Scripts/ShootGlue.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class ShootGlue : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem glueDropplets;
+    [SerializeField] private Transform glueFiller;
+
+    public GameObject glueFillerPrefab;
+
+
+    void Start()
+    {
+        Assert.IsNotNull(glueDropplets);
+        Assert.IsNotNull(glueFiller);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
